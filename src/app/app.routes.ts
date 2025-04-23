93a78444e60c6f6613422cdee00d971528252b41
Pedro J. Molina <pjmolina@metadev.pro>
2025-04-23 17:38:40 +0200

    carga monolotica

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,20 +3,22 @@ import { PlanetListComponent } from './planets/planet-list/planet-list.component
 import { WelcomeComponent } from './welcome/welcome.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { PlanetDetailComponent } from './planets/planet-detail/planet-detail.component';
+import { EjemploComponent } from './ejemplo/ejemplo.component';
 
 export const routes: Routes = [
   { path: '', component: WelcomeComponent },
+  { path: 'ejemplo', component: EjemploComponent },
   {
     path: 'planetas',
     children: [
       { path: '', component: PlanetListComponent },
 
-      { path: ':id', component: PlanetDetailComponent },
-    ],
+      { path: ':id', component: PlanetDetailComponent }
+    ]
   },
-  { path: '**', component: NotFoundComponent },
+  { path: '**', component: NotFoundComponent }
 ];
 
 export const rutasHijas: Routes = [
-  { path: 'detalle/:id', component: PlanetDetailComponent },
+  { path: 'detalle/:id', component: PlanetDetailComponent }
 ];
---
+++ src/app/ejemplo/ejemplo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ejemplo',
+  imports: [],
+  templateUrl: './ejemplo.component.html',
+  styleUrl: './ejemplo.component.scss'
+})
+export class EjemploComponent {
+
+}
