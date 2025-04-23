5d8ef63db446df11aa8d56c1ba820e7ba3890ac2
Pedro J. Molina <pjmolina@metadev.pro>
2025-04-23 19:33:10 +0200

    fib

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-welcome',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './welcome.component.html',
   styleUrl: './welcome.component.scss'
 })
 export class WelcomeComponent {
+  n = 5;
+  resultado = 0;
+  mensaje = 'en reposo';
 
+  constructor(private cd: ChangeDetectorRef) {}
+
+  calcular(): void {
+    this.mensaje = 'calculando...';
+    this.cd.detectChanges();
+
+    this.resultado = fib(+this.n);
+    this.mensaje = 'en reposo';
+    this.cd.detectChanges();
+  }
 }
+
+// mnemoization
+
+const fib = (n: number): number => {
+  if (n < 0) {
+    return -1;
+  }
+  if (n === 0) {
+    return 1;
+  }
+  if (n === 1) {
+    return 1;
+  }
+  return fib(n - 1) + fib(n - 2);
+};
